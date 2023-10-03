beb69a001eca8151c71efa5d4d8554500e7e2931
Ronny Ouma <137153394+Ronni254@users.noreply.github.com>
2023-10-03 21:25:43 +0300

    Add files via upload
    The images, Special names and links should be substituted for something else of your choise. 
    CAUTION!!! I've not included the database file

---
+++ Leave_Application_System/includes/employee-profile-section.php
@@ -0,0 +1,10 @@
+<div class="user-profile pull-right">
+<img class="img" src="../assets/images/icon/logo4.jpg" alt="jpg">
+        <h4 class="user-name dropdown-toggle" data-toggle="dropdown"><?php include 'logged.php' ?> <i class="fa fa-angle-down"></i></h4>
+         <div class="dropdown-menu">
+            <a class="dropdown-item" href="my-profile.php">View Profile</a>
+            <a class="dropdown-item" href="change-password-employee.php">Password</a>
+            <a class="dropdown-item" href="logout.php">Log Out</a>
+     </div>
+</div>
+
---
+++ Leave_Application_System/includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <div class="footer-area">
+                <p>© <?php echo date("Y");?> | Leave Application System | Developed For <a href="https://mftfulfillmentcentre.com/">MFT</a></p>
+        </div>
+</footer>
